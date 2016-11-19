f6fadff04f0b2ce9f916ace3b9d152d8510e98b9
nprok <x4595k4d>
2016-11-19 14:28:00 +0300

    simql 9

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -11,9 +11,9 @@ namespace SimQLTask
     {
         static void Main(string[] args)
         {
-            //var json = Console.In.ReadToEnd();
-            var json =
-                "{\"data\":{\"empty\":{},\"ab\":0,\"x1\":1,\"x2\":2,\"y1\":{\"y2\":{\"y3\":3}}},\"queries\":[\"empty\",\"xyz\",\"x1.x2\",\"y1.y2.z\",\"empty.foobar\"]}";
+            var json = Console.In.ReadToEnd();
+            //var json =
+            //    "{\"data\":{\"empty\":{},\"ab\":0,\"x1\":1,\"x2\":2,\"y1\":{\"y2\":{\"y3\":3}}},\"queries\":[\"empty\",\"xyz\",\"x1.x2\",\"y1.y2.z\",\"empty.foobar\"]}";
             foreach (var result in ExecuteQueries(json))
                 Console.WriteLine(result);
         }
